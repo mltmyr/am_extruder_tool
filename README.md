7276194543ed732d96a0d561f6be8075b6c65307
Andreas <andreas.h.moltumyr@ntnu.no>
2021-09-08 19:28:31 +0200

    Merge package am_extruder_support into am_extruder_tool. Some errors might have resulted from this.

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# extruderHW
+# Custom AM plastic extruder.
+
+### Getting started
+`cd [your_ros2_workspace]/src`
+
+`git clone git@github.com:mltmyr/am_extruder_tool.git`
+(`git clone git@github.com:mltmyr/am_extruder_simple_ui.git`)
+
+### Dependencies
+
+#### RS-232
+`cd am_extruder_tool/`
+
+`git submodule init`
+
+`git submodule update`
+
+#### ros2_control
+`apt install ros-foxy-ros2-control`
+
+`apt install ros-foxy-ros2-controllers`
+
+### Build
+`source /opt/ros/foxy/setup.bash`
+
+`cd [your_ros2_workspace]`
+
+`colcon build`
+
+`source install/setup.bash`
+
+
+### Start extruder demo
+`ros2 launch am_extruder_tool_example.launch.py`
+
+
+### Listen for joint states
+`ros2 topic echo /joint_states`
+
+
+### Send commands to the controllers
+`ros2 topic pub /filament_heater_controller/commands std_msgs/msg/Float64MultiArray 'data: {1.0}'`
+
+`ros2 topic pub /filament_mover_controller/commands std_msgs/Float64MultiArray 'data: {1.0}'`
+
+### (Optionally) Use rqt gui plugin
+Open rqt gui with `rqt` and choose plugin am_extruder/Simple extruder GUI. If the plugin cannot be found in the list, try `rqt --force-discover` instead.
---
+++ launch/am_extruder_tool_example.launch.py
@@ -0,0 +1,77 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+import xacro
+
+def generate_launch_description():
+	description_file_params = {'serial_port': 'ttyS5', 'baud_rate': '9600'}
+
+	extruder_description_path = os.path.join(
+		get_package_share_directory('am_extruder_tool'), 'description', 'am_extruder_tool_example.urdf.xacro')
+	extruder_description_config = xacro.process_file(extruder_description_path, mappings=description_file_params)
+	extruder_description = {'robot_description': extruder_description_config.toxml()}
+
+	extruder_controller = os.path.join(
+		get_package_share_directory('am_extruder_tool'), 'config', 'extruder_controllers.yaml')
+
+	controller_manager_node = Node(
+		package='controller_manager',
+		executable='ros2_control_node',
+		parameters=[extruder_description, extruder_controller],
+		output={
+			'stdout': 'screen',
+			'stderr': 'screen',
+		},
+	)
+
+	extruder_communication_helper_node = Node(
+		package='am_extruder_tool',
+		executable='am_extruder_com',
+		output='both',
+	)
+
+	extruder_state_publisher_node = Node(
+		package='robot_state_publisher',
+		executable='robot_state_publisher',
+		parameters=[extruder_description],
+		output='both',
+		
+	)
+
+	heater_controller_spawner_node = Node(
+		package='controller_manager',
+		executable='spawner.py',
+		parameters=[extruder_controller],
+		arguments=['filament_heater_controller'],
+		output='both',
+	)
+
+	mover_controller_spawner_node = Node(
+		package='controller_manager',
+		executable='spawner.py',
+		parameters=[extruder_controller],
+		arguments=['filament_mover_controller'],
+		output='both',
+	)
+
+	joint_state_controller_spawner_node = Node(
+		package='controller_manager',
+		executable='spawner.py',
+		parameters=[extruder_controller],
+		arguments=['joint_state_controller'],
+		output='both',
+	)
+
+	return LaunchDescription([
+		controller_manager_node,
+		extruder_communication_helper_node,
+		extruder_state_publisher_node,
+		
+		heater_controller_spawner_node,
+		mover_controller_spawner_node,
+		joint_state_controller_spawner_node,
+	])
